7e91425b8d50b4f048e45af5a3b594b64c00da31
Minoru Sakamoto <p2020640@gmail.com>
2016-02-15 00:27:43 +0900

    nemui

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,13 +1,61 @@
 package controllers
 
 import jp.t2v.lab.play2.auth.LoginLogout
+import models.Account
 import play.api._
+import play.api.data.Form
 import play.api.mvc._
+import views.html
+
+import play.api.data.Form
+import play.api.data.Forms._
 
 class Application extends Controller with LoginLogout with AuthConfigImpl {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  /** ログインFormはアプリケーションに応じて自由に作成してください。 */
+  val loginForm = Form {
+    mapping("name" -> text, "password" -> text)
+    //mapping("name" -> text, "password" -> text)(Account.authenticate)(_.map(u => (u.email, "")))
+//      .verifying("Invalid email or password", result => result.isDefined)
+  }
+
+  /** ログインページはアプリケーションに応じて自由に作成してください。 */
+  def login = Action { implicit request =>
+    Ok(html.login(loginForm))
+  }
+
+  /**
+    * ログアウト処理では任意の処理を行った後、
+    * gotoLogoutSucceeded メソッドを呼び出した結果を返して下さい。
+    *
+    * gotoLogoutSucceeded メソッドは Future[Result] を返します。
+    * 以下のようにflashingなどを追加することもできます。
+    *
+    *   gotoLogoutSucceeded.map(_.flashing(
+    *     "success" -> "You've been logged out"
+    *   ))
+    */
+  def logout = Action.async { implicit request =>
+    // do something...
+    gotoLogoutSucceeded
+  }
+
+  /**
+    * ログイン処理では認証が成功した場合、
+    * gotoLoginSucceeded メソッドを呼び出した結果を返して下さい。
+    *
+    * gotoLoginSucceeded メソッドも gotoLogoutSucceeded と同じく Future[Result] を返します。
+    * 任意の処理を追加することも可能です。
+    */
+  def authenticate = Action.async { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => Future.successful(BadRequest(html.login(formWithErrors))),
+      user => gotoLoginSucceeded(user.get.id)
+    )
   }
 
 }
--- app/models/Account.scala
+++ app/models/Account.scala
@@ -1,8 +1,10 @@
 package models
 
+import _root_.scalikejdbc.DBSession
 import anorm.SqlParser._
 import anorm._
 import com.sun.xml.internal.bind.v2.TODO
+import org.mindrot.jbcrypt.BCrypt
 import play.api.db.DB
 
 /**
@@ -12,7 +14,7 @@ case class Account(id: Int, password: String, name: String, role: Role)
 
 object Account {
 
-  val task = {
+  val account = {
     get[Int]("id") ~
       get[String]("password") ~
       get[String]("name") ~
@@ -20,11 +22,16 @@ object Account {
       case id ~ password ~ name ~ role => Account(id,password,name,role )
     }
   }
+  def authenticate(name: String, password: String): Option[Account] =DB.withConnection { implicit c =>
+    SQL("select * from account where {name}").on('name -> name).as(account *).find(_.password==password)
+  }
 
   def findByIdAsync(id: Int)= Some(Account(1,"password","name",Administrator))
 
+  def findByName(name: String)= Some(Account(1,"password","name",Administrator))
+
   def findAll()= DB.withConnection { implicit c =>
-    SQL("select * from task").as(task *)
+    SQL("select * from account").as(account *)
   }
 
   def create(account: Account) = {}
